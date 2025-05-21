d12601cd498d739774f0375acf1cedd0e03684bd
Percy0506 <ngyuisik@gmail.com>
2025-05-21 12:14:27 -0700

    all fixed

--- HTMLManager.java
+++ HTMLManager.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class HTMLManager {
   private Queue<HTMLTag> tags;
   
-  public void HTMLManager(Queue<HTMLTag> html) {
+  public HTMLManager(Queue<HTMLTag> html) {
      if(html == null) {
         throw new IllegalArgumentException("The html is null");
      }
@@ -19,11 +19,39 @@ public class HTMLManager {
   
   public String toString() {
      String result = "";
-     while(!tags.isEmpty()) {
-        HTMLTag tag = tags.remove();
-        result += tag.toString() + " ";   
+     for(HTMLTag tag : tags) {
+        result += tag.toString().trim() + " ";   
      }
      return result;
   }
   
+  public void fixHTML() {
+     int size = tags.size();
+     Stack<HTMLTag> result = new Stack<>();
+     for(int i = 0; i < size; i++) {
+        HTMLTag tag = tags.remove();
+        if(tag.isOpening()) {
+           result.push(tag);
+           tags.add(tag);
+        } else if (tag.isSelfClosing()) {
+           tags.add(tag);
+        } else if (tag.isClosing() && result.isEmpty()) {
+          //nothing to do here in this case
+        } else if (tag.isClosing() && !result.peek().equals(tag)) {
+           HTMLTag add = result.pop();
+           HTMLTag correct = add.getMatching();
+           tags.add(correct);
+        } else if (tag.isClosing() && result.peek().equals(tag)) {
+          result.pop();
+          tags.add(tag);
+        }
+     }
+     while(!result.isEmpty()) {
+        HTMLTag add = result.pop();
+        HTMLTag correct = add.getMatching();
+        tags.add(correct);        
+     }
+  }
+  
 }
+
