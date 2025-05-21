1078242e56523eea276887606c3483879d9f9e4a
Percy0506 <ngyuisik@gmail.com>
2025-05-21 11:17:24 -0700

    HTMLManager done

--- HTMLManager.java
+++ HTMLManager.java
@@ -3,4 +3,13 @@ import java.util.*;
 public class HTMLManager {
   private Queue<HTMLTag> tags;
   
+  public void HTMLManager(Queue<HTMLTag> html) {
+     if(html == null) {
+        throw new IllegalArgumentException("The html is null");
+     }
+     while(!html.isEmpty()) {
+        tags.add(html.remove());
+     }
+  }
+  
 }
